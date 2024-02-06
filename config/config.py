d67271d4cbb0b4e2ca45c1a007136a3f2c46e618
macrdel <kutdusoff14@gmail.com>
2024-02-06 13:37:55 +0300

    create config.py

---
+++ config/config.py
@@ -0,0 +1,7 @@
+API_KEY = "AIzaSyBRDNKH6e6GJy6l4hf-c8YcdX-Jzxi3HAM"
+
+sentiment_model = "MonoHime/rubert-base-cased-sentiment-new"
+sum_model = "IlyaGusev/mbart_ru_sum_gazeta"
+
+PATH_DATA = "data"
+NAME_DATA = "dataset_sentiments.csv"
