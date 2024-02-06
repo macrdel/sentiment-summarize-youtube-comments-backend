8843bcd9aeb83820fb38ca37c72e36d8e7ba33e1
macrdel <kutdusoff14@gmail.com>
2024-02-06 14:45:41 +0300

    import necessary libraries & add root hadler and get comments hadler

---
+++ app/api.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import config
+from pydantic import BaseModel
+from transformers import pipeline
+import uvicorn
+
+sentiment_model = pipeline(config.sentiment_model)
+app = FastAPI()
+
+class YouTubeUrl(BaseModel):
+    url_video: str
+
+@app.get('/')
+def read_root():
+    return {'message': 'FastAPI+HuggingFace app sentiment + summarize YouTube comments'}
+
+@app.post('/comments')
+def get_comments(url_video: YouTubeUrl):
+    data = pipeline_sentiment(url_video.url_video, config.API_KEY, sentiment_model)
+    data.to_csv(f"{config.DATA_FILE}", index=False)
+    return {'message': 'Success'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=80)
