3164527b316d3bb6409cf9ae158db52cdb291927
macrdel <kutdusoff14@gmail.com>
2024-02-07 23:11:45 +0300

    add handle of pipeline summarization

--- app/api.py
+++ app/api.py
@@ -1,5 +1,5 @@
 import config.config as config
-from app.src.src import pipeline_sentiment, pipeline_stats
+from app.src.src import pipeline_sentiment, pipeline_stats, pipeline_summarize
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -9,6 +9,8 @@ import pandas as pd
 import os
 
 sentiment_model = pipeline(model=config.sentiment_model)
+sum_model = pipeline(model=config.sum_model, use_fast=True)
+
 app = FastAPI()
 
 class YouTubeUrl(BaseModel):
@@ -29,6 +31,12 @@ def get_stats_sent():
     if f"{config.NAME_DATA}" in os.listdir(f"{config.PATH_DATA}"):
         data = pd.read_csv(f"{config.DATA_FILE}")
         return pipeline_stats(data)
+
+@app.get('/summarization')
+def get_summarize():
+    if f"{config.NAME_DATA}" in os.listdir(f"{config.PATH_DATA}"):
+        data = pd.read_csv(f"{config.DATA_FILE}")
+        return pipeline_summarize(data['text_comment'], sum_model)
 
 
 if __name__ == '__main__':
