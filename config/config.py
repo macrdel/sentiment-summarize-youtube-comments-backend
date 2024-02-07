6d21ebb93e2026f9177952c1845ee7038c22ae19
macrdel <kutdusoff14@gmail.com>
2024-02-07 22:11:50 +0300

    update config.py

--- config/config.py
+++ config/config.py
@@ -4,3 +4,5 @@ sentiment_model = "MonoHime/rubert-base-cased-sentiment-new"
 sum_model = "IlyaGusev/mbart_ru_sum_gazeta"
 
 DATA_FILE = "data/dataset_sentiments.csv"
+PATH_DATA = "data"
+NAME_DATA = "dataset_sentiments.csv"
