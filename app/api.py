593cc35a0560c4c48a6dd97ebed5519340327404
macrdel <kutdusoff14@gmail.com>
2024-02-13 00:27:40 +0300

    update api.py

--- app/api.py
+++ app/api.py
@@ -1,15 +1,18 @@
-import config.config as config
+from config import config, secrets
 from app.src.src import pipeline_sentiment, pipeline_stats, pipeline_summarize
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 # from transformers import pipeline
-import uvicorn
+# import uvicorn
 import pandas as pd
 import os
 
 # sentiment_model = pipeline(model=config.sentiment_model)
 # sum_model = pipeline(model=config.sum_model, use_fast=True)
+headers = {"Authorization": f"Bearer {secrets.API_TOKEN}"}
+SENT_API_URL = f"https://api-inference.huggingface.co/models/{config.sentiment_model}"
+SUM_API_URL = f"https://api-inference.huggingface.co/models/{config.sum_model}"
 
 app = FastAPI()
 
@@ -22,9 +25,9 @@ def read_root():
 
 @app.post('/comments')
 def get_comments(url_video: YouTubeUrl):
-    data = pipeline_sentiment(url_video.url_video, config.API_KEY, sentiment_model)
+    data = pipeline_sentiment(url_video.url_video, config.API_KEY, headers, SENT_API_URL)
     data.to_csv(f"{config.DATA_FILE}", index=False)
-    return {'message': 'Success'}
+    return data # {'message': 'Success'}
 
 @app.get('/stats')
 def get_stats_sent():
@@ -36,8 +39,8 @@ def get_stats_sent():
 def get_summarize():
     if f"{config.NAME_DATA}" in os.listdir(f"{config.PATH_DATA}"):
         data = pd.read_csv(f"{config.DATA_FILE}")
-        return pipeline_summarize(data['text_comment'], sum_model)
+        return pipeline_summarize(data['text_comment'], headers, SUM_API_URL)
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=80)
+#if __name__ == '__main__':
+#    uvicorn.run(app, host='127.0.0.1', port=80)
