ff226b6447ee471ca2d979b0de98763a962697b8
macrdel <kutdusoff14@gmail.com>
2024-06-07 01:47:50 +0300

    update workflow

--- app/api.py
+++ app/api.py
@@ -48,7 +48,7 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"] ,
 )
 
 Instrumentator().instrument(app).expose(app)
